58e5fd2ae471346405e7f0e2e20b7b25d9ec5c2f
o3ohanas <o3ohanas@gmail.com>
2021-09-15 23:42:10 +0900

    ADD : BFS, DP, MST

---
+++ DP/HN_BOJ1463.java
@@ -0,0 +1,27 @@
+package com.ssafy.dp;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class HN_BOJ1463 {
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int X = Integer.parseInt(br.readLine());
+		
+		int cnt[] = new int[X+1];
+		cnt[1] = 0 ;
+		
+		for (int x = 2; x <= X; x++) {
+			cnt[x] = cnt[x-1] +1;
+			if(x % 3 == 0) {
+				cnt[x] = Math.min(cnt[x], cnt[x/3] + 1);
+			}
+			if(x % 2 == 0) {
+				cnt[x] = Math.min(cnt[x], cnt[x/2] + 1);
+			}
+		}
+		System.out.println(cnt[X]);
+	}
+
+}
